2fd8f8b6d4a8407bc52f5167612d64e27a7eb061
Joshua | Tommaso <87675824+JoshuaKasa@users.noreply.github.com>
2024-02-26 22:16:10 +0100

    Updated commands

--- emulator_command.py
+++ emulator_command.py
@@ -7,12 +7,12 @@ NES_PATH = r'C:\Users\jizos\Documents\Emulators\NES' # NES = NES emulator
 
 # We will use all of this path 
 EMULATORS_COMMANDS = {
-    DUCKSTATION_PATH: 'duckstation -fullscreen --  ',
-    MAME_PATH: 'mame',
-    NES_PATH: 'nes'
+    'PS1': 'duckstation -fullscreen --  ',
+    'Arcade': 'mame64 ',
+    'NES': 'nestopia '
 }
 EMULATORS_PATH = {
-    'PS1': EMULATORS_COMMANDS[DUCKSTATION_PATH], 
-    'Arcade': EMULATORS_COMMANDS[MAME_PATH],
-    'NES': EMULATORS_COMMANDS[NES_PATH]
+    DUCKSTATION_PATH: EMULATORS_COMMANDS['PS1'],
+    MAME_PATH: EMULATORS_COMMANDS['Arcade'],
+    NES_PATH: EMULATORS_COMMANDS['NES']
 }
